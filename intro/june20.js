ffe1c24d89f004e1dcfcd58d4ad093ee01785c58
davidajimati <ajimatidavid@gmail.com>
2023-07-14 11:43:28 +0100

    file cleanups

--- intro/june20.js
+++
@@ -1,9 +0,0 @@
-// Find the sum of the following numbers
-let a = 30
-let b = 21
-let c = 40
-let d = 32
-let e = 12
-
-let answer = a + b + c + d + e;
-console.log("The answer is:",answer);
--- project/area/area.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector("#button");
-
-button.onclick = () => {
-  const height = Number(document.querySelector("#height").value);
-  const width = Number(document.querySelector("#width").value);
-  let ansBox = document.querySelector("#answer");
-  ansBox.textContent = height * width;
-}
